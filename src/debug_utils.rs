7936ae7c45d6ffd88b76eaef931d977103efdd4e
pi4erd <piuuttri@gmail.com>
2026-01-05 14:48:23 +0100

    cleanup: debug utils values
    feat: add Display for debug utils flags
    
    also added test for it

--- src/debug_utils.rs
+++ src/debug_utils.rs
@@ -1,20 +1,22 @@
+use std::fmt::Display;
+
 use ash::vk;
 use bitflags::bitflags;
 
 bitflags! {
     #[derive(Debug, Clone, Copy, PartialEq, Eq)]
     pub struct DebugUtilsMessageSeverity: u32 {
-        const VERBOSE = 0b1;
-        const INFO = 0b1_0000;
-        const WARNING = 0b1_0000_0000;
-        const ERROR = 0b1_0000_0000_0000;
+        const VERBOSE = vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE.as_raw();
+        const INFO = vk::DebugUtilsMessageSeverityFlagsEXT::INFO.as_raw();
+        const WARNING = vk::DebugUtilsMessageSeverityFlagsEXT::WARNING.as_raw();
+        const ERROR = vk::DebugUtilsMessageSeverityFlagsEXT::ERROR.as_raw();
     }
 
     #[derive(Debug, Clone, Copy, PartialEq, Eq)]
     pub struct DebugUtilsMessageType: u32 {
-        const GENERAL = 0b1;
-        const VALIDATION = 0b10;
-        const PERFORMANCE = 0b100;
+        const GENERAL = vk::DebugUtilsMessageTypeFlagsEXT::GENERAL.as_raw();
+        const VALIDATION = vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION.as_raw();
+        const PERFORMANCE = vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE.as_raw();
     }
 }
 
@@ -30,6 +32,36 @@ impl Into<vk::DebugUtilsMessageTypeFlagsEXT> for DebugUtilsMessageType {
     }
 }
 
+impl Display for DebugUtilsMessageSeverity {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let bit_count = self.iter().count();
+        for (i, (name, _)) in self.iter_names().enumerate() {
+            write!(f, "{}", name)?;
+
+            if i < bit_count - 1 {
+                write!(f, " | ")?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl Display for DebugUtilsMessageType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let bit_count = self.iter().count();
+        for (i, (name, _)) in self.iter_names().enumerate() {
+            write!(f, "{}", name)?;
+
+            if i < bit_count - 1 {
+                write!(f, " | ")?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
 pub struct DebugUtilsCallbackData<'a> {
     pub message: &'a str,
     // TODO: Expand the class, if needed
@@ -40,3 +72,26 @@ pub type DebugCallback = fn(
             DebugUtilsMessageType,
             &DebugUtilsCallbackData
         ) -> bool;
+
+#[cfg(test)]
+mod tests {
+    use crate::debug_utils::*;
+
+    #[test]
+    fn severity_type_display_test() {
+        let single_sev = DebugUtilsMessageSeverity::ERROR;
+        assert_eq!(format!("{}", single_sev), "ERROR");
+
+        let multi_sev = DebugUtilsMessageSeverity::WARNING |
+            DebugUtilsMessageSeverity::ERROR;
+        // NOTE: Order is the same as defined in struct
+        assert_eq!(format!("{}", multi_sev), "WARNING | ERROR");
+
+        let single_type = DebugUtilsMessageType::GENERAL;
+        assert_eq!(format!("{}", single_type), "GENERAL");
+
+        let multi_type = DebugUtilsMessageType::GENERAL |
+            DebugUtilsMessageType::VALIDATION;
+        assert_eq!(format!("{}", multi_type), "GENERAL | VALIDATION");
+    }
+}
